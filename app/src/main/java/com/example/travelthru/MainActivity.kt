150bebaddfa33d37cde74d5671e623237fd6a529
Skyler <xla276@sfu.ca>
2022-03-14 00:00:44 -0700

    id

--- app/src/main/java/com/example/travelthru/MainActivity.kt
+++ app/src/main/java/com/example/travelthru/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.sql.Time
+import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +21,10 @@ class MainActivity : AppCompatActivity() {
         if (bundle!=null){
             val name = bundle.getString("cityName") // 1
             if (name != null) {
-                cities.add(CityTimeCard(name))
+                cities.add(CityTimeCard(zoneId(name)))
             }
         } else {
-            cities.add(CityTimeCard("Asia/Tokyo"))
+            cities.add(CityTimeCard(zoneId("Tokyo")))
         }
 
         val addCity = findViewById<Button>(R.id.add_city)
@@ -35,5 +37,14 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = RecyclerAdapter(cities)
         recyclerView.adapter = adapter
+    }
+
+    private fun zoneId(name: String): String {
+        for(i in 0..TimeZone.getAvailableIDs().size) {
+            if (name in TimeZone.getAvailableIDs()[i]) {
+                return TimeZone.getAvailableIDs()[i]
+            }
+        }
+        return name
     }
 }
